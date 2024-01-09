a9621b4ce2e990904de1ede79cf2be88e4385e24
TheMalling <155809058+TheMalling@users.noreply.github.com>
2024-01-09 23:38:19 +0000

    Add files via upload

--- Untitled Banban Mod/scripts/hud.lua
+++ Untitled Banban Mod/scripts/hud.lua
@@ -1,3 +1,21 @@
-function onUpdate()
-    setTextString('botplaytxt', 'BANBANNED')
+luaDebugMode = true
+
+function onCreate()
+    makeLuaText('watermark', songName, 0, 0, 680)
+    setTextSize('watermark', 20)
+    setTextAlignment('watermark', left)
+
 end
+
+function onCreatePost()
+    setProperty('timeBar.color', getColorFromHex("313AFF"))
+end
+
+function onCountdownStarted()
+    addLuaText('watermark')
+end
+
+
+
+
+
