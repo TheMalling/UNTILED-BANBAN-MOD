76210b49e2aa8b9844f865945fbf58eef15c98db
TheMalling <155809058+TheMalling@users.noreply.github.com>
2024-01-13 23:10:19 +0000

    Add files via upload

--- Untitled Banban Mod/scripts/hud.lua
+++ Untitled Banban Mod/scripts/hud.lua
@@ -1,21 +1,37 @@
 luaDebugMode = true
 
+
+
 function onCreate()
-    makeLuaText('watermark', songName, 0, 0, 680)
+    
+    --watermark
+    
+    makeLuaText('watermark', songName, 0, 0, 700)
     setTextSize('watermark', 20)
     setTextAlignment('watermark', left)
-
 end
 
-function onCreatePost()
-    setProperty('timeBar.color', getColorFromHex("313AFF"))
-end
+
 
 function onCountdownStarted()
+    
+    --adds watermark
+    
     addLuaText('watermark')
 end
 
+--new timebar colour system (so much better)
 
+function onUpdate()
+    dadColR = getProperty('dad.healthColorArray[0]')
+        dadColG = getProperty('dad.healthColorArray[1]')
+        dadColB = getProperty('dad.healthColorArray[2]')
+        dadColFinal = string.format('%02x%02x%02x', dadColR, dadColG, dadColB)
+        setTimeBarColors(dadColFinal, "000000")
 
+        --ban-play
+        
+        setTextString("botplayTxt", "BANPLAY")
+end
 
 
